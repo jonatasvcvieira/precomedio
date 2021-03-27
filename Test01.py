8119b7178e863be36450d56f2e48a6c9e8716d8b
jvasconcelosdev <jonatasvieir@gmail.com>
2021-03-27 12:35:51 -0300

    Test1.1

--- Test01.py
+++ Test01.py
@@ -5,4 +5,11 @@ v2 = float(input('Digite o valor da segunda compra: R$'))
 m1 = q1 * v1
 m2 = q2 * v2
 pm = (m1+m2)/(q1+q2)
+novoq1 = q1 + q2
+novov1 = pm
+while
+opção = str(input('Deseja calcular mais um preço médio? [S/N] ')).strip().upper()[0]
+    opção = str(input(''))
+
 print('O preço medio é R${:.2f}'.format(pm))
+print('Agora você tem {} com preço médio de {:.2f}'.format(novoq1, novov1))
