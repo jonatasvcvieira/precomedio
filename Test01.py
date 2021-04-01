468c28d352a193de77df5297412050cae9880d0e
jvasconcelosdev <jonatasvieir@gmail.com>
2021-04-01 18:49:31 -0300

    Atualização

--- Test01.py
+++ Test01.py
@@ -1,3 +1,7 @@
+from time import sleep
+print('**** PREÇO MÉDIO ****')
+print('=-=' * 7)
+sleep(2)
 q1 = float(input('Digite a quantidade da primeira compra:'))
 v1 = float(input('Digite o valor da primeira compra: R$'))
 q2 = float(input('Digite a quantidade da segunda compra:'))
@@ -5,13 +9,32 @@ v2 = float(input('Digite o valor da segunda compra: R$'))
 m1 = q1 * v1
 m2 = q2 * v2
 pm = (m1+m2)/(q1+q2)
-novoq1 = q1 + q2
-novov1 = pm
-'''opção = str(input('Deseja calcular mais um preço médio? [S/N] ')).strip().upper()[0]
-while opção not in 'SsNn':
-    opção = str(input('Digite um valor: '))
-        print('Seu novo total de moedas é {}'.format(novoq1))
-        print('Seu novo preço médio é {}'.format_map(novov1))'''
+opção = 0
+while opção != 3:
+    print('''    
+    [ 1 ] Mostrar preço médio
+    [ 2 ] Calcular outro preço médio
+    [ 3 ] Sair
+    ''')
+    opção = int(input('>>>>> Qual é a sua opção?'))
+    if opção == 1:
+        m1 = q1 * v1
+        m2 = q2 * v2
+        qtotal = q1 + q2
+        pm = (m1 + m2) / (q1 + q2)
+        print('A quantidade total é {:.4f} com preço medio de R${:.4f}'.format(qtotal, pm))
+    elif opção == 2:
+        print('Informe os valores novamente:')
+        q1 = float(input('Digite a quantidade da primeira compra:'))
+        v1 = float(input('Digite o valor da primeira compra: R$'))
+        q2 = float(input('Digite a quantidade da segunda compra:'))
+        v2 = float(input('Digite o valor da segunda compra: R$'))
+    elif opção == 3:
+        print('Finalizando...')
+    else:
+        print('Opção inválida. Tente novamente!')
+    print('=-=' * 10)
+    sleep(4)
+print('Fim do programa!')
 
-#print('O preço medio é R${:.2f}'.format(pm))
-print('Agora você tem {:.4f} moedas com preço médio de R${:.4f}'.format(novoq1, novov1))
+
